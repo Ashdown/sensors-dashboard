74311590ba1cfa0e0f5df7750a105749ac6848e5
Rory Devane <rory.devane@gmail.com>
2017-04-16 21:22:20 +0100

    fixed es lint errors

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+
 var express = require("express");
 var app = express();
 
 app.set("port", (process.env.PORT || 3000));
 
 app.get("/sensors", function (req, res) {
-	res.setHeader("Content-Type", "application/json");
-	res.send(JSON.stringify({a: 1}));
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({a: 1}));
 });
 
-app.listen(app.get("port"), function() {
-	console.log("Node app is running on port", app.get("port"));
+app.listen(app.get("port"), function () {
+    console.log("Node app is running on port", app.get("port"));
 });
 
 
