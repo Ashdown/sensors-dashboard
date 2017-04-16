da45e8062dadafb3f1b4e274f02d5d8596abdd22
Rory Devane <rory.devane@gmail.com>
2017-04-16 22:22:29 +0100

    added /sensors endpoint

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-
-var express = require("express");
-var app = express();
+let express = require("express");
+let app = express();
 
 app.set("port", (process.env.PORT || 3000));
 
 app.get("/sensors", function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.send(JSON.stringify({a: 1}));
+    let sensorsData = require("./data/sensors.json");
+    res.send(sensorsData);
 });
 
 app.listen(app.get("port"), function () {
     console.log("Node app is running on port", app.get("port"));
 });
-
 
 module.exports = app;
 
---
+++ test/feature/sensors-test.js
@@ -0,0 +1,41 @@
+/*global describe, it*/
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let app = require("../../app");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Sensors", function () {
+    it("should return a 200 response", function (done) {
+        chai.request(app)
+            .get("/sensors")
+            .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+    it("should return a valid json response", function (done) {
+        chai.request(app)
+            .get("/sensors")
+            .end(function (err, res) {
+                let isValidJson = true;
+                if (!/^[\[|\{](\s|.*|\w)*[\]|\}]$/.test(res.body)) {
+                    isValidJson = false;
+                }
+                isValidJson.should.equal(true);
+                done();
+            });
+    });
+    it("should return a list of 3 sensors", function (done) {
+        chai.request(app)
+            .get("/sensors")
+            .end(function (err, res) {
+                let sensors = res.body;
+                console.log(sensors);
+                (sensors.length).should.equal(3);
+                done();
+            });
+    });
+});
