3be50c88c5b948c1b6ccca17b55c94275bf3f743
Rory Devane <rory.devane@gmail.com>
2017-04-16 21:14:08 +0100

    updated app.js with heroku settings

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var app = express();
 
+app.set("port", (process.env.PORT || 3000));
+
 app.get("/sensors", function (req, res) {
 	res.setHeader("Content-Type", "application/json");
 	res.send(JSON.stringify({a: 1}));
 });
 
-app.listen(3000);
+app.listen(app.get("port"), function() {
+	console.log("Node app is running on port", app.get("port"));
+});
+
 
 module.exports = app;
 
