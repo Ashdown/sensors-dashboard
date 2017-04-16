0732497c84065b2d48b7253bfeab9567e06812f5
Rory Devane <rory.devane@gmail.com>
2017-04-16 21:06:05 +0100

    cleaned up lint errors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/sensors', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({a: 1}));
+app.get("/sensors", function (req, res) {
+	res.setHeader("Content-Type", "application/json");
+	res.send(JSON.stringify({a: 1}));
 });
 
 app.listen(3000);
