ea18a7b48fefe8d47ca0d5c329c67f86480f7f0a
Rory Devane <rory.devane@gmail.com>
2017-04-18 04:13:20 +0100

    small fixes, and final build

--- test/unit/DataItem-spec.js
+++ test/unit/DataItem-spec.js
@@ -7,9 +7,9 @@ import DataItem from "../../src/components/DataItem";
 function setup() {
 
     const props = {
-        time: "1234",
+        time: "0",
         value: "12.3"
-    }
+    };
 
     const enzymeWrapper = shallow(<DataItem {...props}/>);
 
@@ -24,7 +24,7 @@ describe("DataItem Component", () => {
     it("should render self and subcomponents", () => {
         const { enzymeWrapper, props } = setup();
         expect(enzymeWrapper.find(".data-item").hasClass("data-item")).toBe(true);
-        expect(enzymeWrapper.find(".time").text()).toBe(props.time);
+        expect(enzymeWrapper.find(".time").text()).toBe("1 Jan 1970 0:0:0");
         expect(enzymeWrapper.find(".value").text()).toBe(props.value);
     });
 });
